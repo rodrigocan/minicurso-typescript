c419951e13be252d863a1fa77651b039af45e4d2
Rodrigo Candido <rodrigo@DESKTOP-U09JB93.localdomain>
2021-01-11 18:16:04 -0300

    Final da aula 05

---
+++ types.ts
@@ -0,0 +1,52 @@
+// boolean (true / false)
+let isOpen: boolean
+isOpen = true
+
+// string ('foo', "foo", `foo`)
+let message: string
+message = `foo ${isOpen}`
+
+// number (int, float, hex, binary)
+let total: number
+total = 0xff0
+
+// array (type[])
+let items: string[]
+items = ["foo", "bar"]
+
+let values: Array<number>
+values = [1, 2, 3]
+
+// tuple
+let title: [number, string, string]
+title = [1, "foo", "bar"]
+
+// enum
+enum Colors {
+  white = '#fff',
+  black = '#000'
+}
+
+// any (qualquer coisa) NÃO É LEGAL!
+let coisa: any
+coisa = [1]
+
+// void (vazio)
+function logger(): void {
+  console.log('foo')
+}
+
+// null | undefined
+type Bla = string | undefined
+
+// never
+function error(): never {
+  throw new Error("error")
+}
+
+// object
+let cart: object
+
+cart = {
+  key: "fi"
+}
